84df87db99026e6da42281e5aaf9720ec1b7b18e
Vinícius Pereira <jedipereira@gmail.com>
2017-07-14 10:57:41 -0300

    add SimpleCov

--- Gemfile
+++ Gemfile
@@ -2,11 +2,16 @@ source 'https://rubygems.org'
 
 ruby '~>2.4'
 
-gem 'cucumber', '~>2.4.0'
 gem 'httparty', '~>0.15.5'
 gem 'puma', '~>3.9.1'
-gem 'rspec', '~>3.6.0'
 gem 'shotgun', '~>0.9.2'
 gem 'sinatra', '~>2.0.0'
 gem 'sinatra-contrib', '~>2.0.0'
 gem 'webmock', '~>2.3.2'
+
+group :test do
+  gem 'codeclimate-test-reporter', '~> 1.0.0'
+  gem 'cucumber', '~>2.4.0'
+  gem 'rspec', '~>3.6.0'
+  gem 'simplecov', '~>0.14.1'
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,34 +1,9 @@
 require_relative "#{Dir.pwd}/controllers/tweets.rb"
 
-# require 'capybara'
-# require 'capybara/cucumber'
-# require 'capybara/dsl'
-# require 'capybara/poltergeist'
 require 'httparty'
 require 'rspec'
-# require 'selenium-webdriver'
-# require 'site_prism'
-# require 'webmock/cucumber'
+require 'simplecov'
 require 'webmock/cucumber'
 
+SimpleCov.start
 WebMock.allow_net_connect!
-
-# Capybara.default_max_wait_time = 10
-# Capybara.default_driver = :poltergeist
-
-# Capybara.register_driver :selenium do |app|
-#     Capybara::Selenium::Driver.new(app, :browser => :chrome)
-# end
-
-# Capybara.register_driver :poltergeist do |app|
-#     options = {
-#         :js_errors => false,
-#         :timeout => 10,
-#         # :debug => true,
-#         # :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
-#         # :inspector => true
-#     }
-#     Capybara::Poltergeist::Driver.new(app, options)
-# end
-
-# World(Capybara)
